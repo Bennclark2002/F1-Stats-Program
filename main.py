87c8367e8d4d75342a12d2d8c1eca6dc4a08d38f
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2023-10-16 23:39:47 -0500

    Add files via upload
    Updated Main.py for the start of the API connection

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import requests
+import xml.etree.ElementTree as ET
 
-response = requests.get("http://ergast.com/api/f1/drivers?=123")
+url = "http://ergast.com/api/f1/drivers"
+params = {"param_name": "123"}
 
-print(response.status_code)
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    print("XML Response:")
+    print(response.text)  # Print the XML response for analysis
+
+    root = ET.fromstring(response.text)
+    driver = root.find(".//Driver")  # Find the first driver element, adjust this according to your XML structure
+    
+    if driver is not None:
+        Firstname = driver.find("GivenName").text
+        Lastname = driver.find("FamilyName").text
+        DOB = driver.find("DateOfBirth").text
+        Nationality = driver.find("Nationality").text
+
+        print(f"{Firstname} {Lastname} was born on {DOB} in the country of {Nationality}")
+    else:
+        print("Driver data not found in the XML response.")
+
+else:
+    print(f"Error: {response.status_code}")
